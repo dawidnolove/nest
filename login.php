761e1f3a8e2820129471c4e4341b6e61a4553dfd
Kolsowiasty <143624030+Kolsowiasty@users.noreply.github.com>
2025-03-27 21:18:45 +0100

    Update login.php
    Poprawiona wersja kompatybilna z nowym index.php

--- login.php
+++ login.php
@@ -1,17 +1,19 @@
 <?php
-session_start();
+
 require_once 'db.php';
+
+session_start();
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['mail']);
     $password = $_POST['password'];
 
     try {
-        $stmt = $pdo->prepare("SELECT id, password FROM users WHERE email = ?");
+        $stmt = $pdo->prepare("SELECT id, email, password  FROM users WHERE email = ?");
         $stmt->execute([$email]);
         $user = $stmt->fetch();
 
-        if ($user && password_verify($password, $user['password'])) {
+        if ($user && password_verify($password, $user['password'])) {         
             $_SESSION['user_id'] = $user['id'];
             header("Location: index.php");
             exit();
@@ -24,4 +26,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 } else {
     echo "Nieprawidłowa metoda żądania.";
 }
+
 ?>
