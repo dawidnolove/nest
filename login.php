53a1381d1fd48f388a29696e4649f67c9a1a9110
SzymKacz <126461711+SzymKacz@users.noreply.github.com>
2025-04-22 13:12:17 +0200

    login.php

--- login.php
+++ login.php
@@ -1,35 +1,47 @@
 <?php
-require_once 'db.php';
 session_start();
-
+include("db.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['mail']);
     $password = $_POST['password'];
 
     try {
-        $stmt = $pdo->prepare("SELECT id, password, email, phone FROM users WHERE email = ?");
-        $stmt->execute([$email]);
+        $stmt = $pdo->prepare("SELECT id, password, email, phone FROM admins WHERE email = :email");
+        $stmt->execute([':email' => $email]);
+        $admin = $stmt->fetch();
+
+        if ($admin && password_verify($password, $admin['password'])) {
+            $_SESSION['email'] = $admin['email'];
+            $_SESSION['is_admin'] = true; 
+            $_SESSION['message'] = "Zalogowano jako administrator";
+            header("Location: home.php");
+            exit();
+        }
+        $stmt = $pdo->prepare("SELECT id, password, email, phone FROM users WHERE email = :email");
+        $stmt->execute([':email' => $email]);
         $user = $stmt->fetch();
 
         if ($user && password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['email'] = $user['email'];
             $_SESSION['phone'] = $user['phone'];
+            $_SESSION['is_admin'] = false;
+            $_SESSION['message'] = "Zalogowano jako użytkownik";
             header("Location: home.php");
             exit();
-        } else {
-            $_SESSION['message'] = "Błędny email lub hasło";
-            header("Location: login-form.php"); // Zmień na .php
-            exit();
         }
+        $_SESSION['message'] = "Błędny e-mail lub hasło.";
+        header("Location: login-form.php");
+        exit();
+
     } catch (PDOException $e) {
         $_SESSION['message'] = "Błąd: " . $e->getMessage();
-        header("Location: login-form.php"); // Zmień na .php
+        header("Location: login-form.php");
         exit();
     }
 } else {
     $_SESSION['message'] = "Nieprawidłowa metoda żądania.";
-    header("Location: login-form.php"); // Zmień na .php
+    header("Location: login-form.php");
     exit();
 }
 ?>
